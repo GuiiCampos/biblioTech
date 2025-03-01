a38797c1942d0d4cefea9bae9f13ffae09c7007f
Guilherme Campos <guiicampos31@gmail.com>
2025-03-01 17:18:16 -0300

    Classe livro

---
+++ src/Livro.java
@@ -0,0 +1,37 @@
+public class Livro {
+    private String nome;
+    private String genero;
+    private int anoLancamento;
+
+    public Livro() {
+    }
+    public Livro(String nome, String genero, int anoLancamento) {
+        this.nome = nome;
+        this.genero = genero;
+        this.anoLancamento = anoLancamento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+}
